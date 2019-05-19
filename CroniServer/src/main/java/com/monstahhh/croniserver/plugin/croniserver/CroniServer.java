95e16ac3f3217156050697aaf771010ec19f95da
Thybe Van Beersel <vanbeerselthybe@gmail.com>
2019-05-19 17:47:12 +0200

    s

--- CroniServer/src/main/java/com/monstahhh/croniserver/plugin/croniserver/CroniServer.java
+++ CroniServer/src/main/java/com/monstahhh/croniserver/plugin/croniserver/CroniServer.java
@@ -3,8 +3,13 @@ package com.monstahhh.croniserver.plugin.croniserver;
 import com.monstahhh.croniserver.plugin.croniserver.commands.DistanceCommand;
 import com.monstahhh.croniserver.plugin.croniserver.commands.HubCommand;
 import com.monstahhh.croniserver.plugin.croniserver.events.PlayerListener;
+import net.minecraft.server.v1_14_R1.MinecraftServer;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginLogger;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
@@ -13,14 +18,39 @@ public final class CroniServer extends JavaPlugin {
     @Override
     public void onEnable() {
 
+        PluginLogger logger = new PluginLogger(this);
+
         getServer().getPluginManager().registerEvents(new PlayerListener(), this);
 
         this.getCommand("hub").setExecutor(new HubCommand());
+        this.getCommand("spawn").setExecutor(new HubCommand());
         this.getCommand("distance").setExecutor(new DistanceCommand());
         this.getCommand("pdistance").setExecutor((new DistanceCommand()));
         this.getCommand("lol").setExecutor(new DistanceCommand());
 
-        PluginLogger logger = new PluginLogger(this);
-        logger.log(new LogRecord(Level.INFO, "Loaded CroniServer Build 14/05/19"));
+        timer();
+        logger.log(new LogRecord(Level.INFO, "Loaded TPS Recorder"));
+        logger.log(new LogRecord(Level.INFO, "Loaded CroniServer Build 19/05/19"));
+    }
+
+    public void timer()
+    {
+        this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable()
+                {
+                    public void run()
+                    {
+                        for (double tps : MinecraftServer.getServer().recentTps)
+                        {
+                            if (tps <= 18.75) {
+                                for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+                                    if (p.hasPermission("croniserver.admin")) {
+                                        p.sendMessage(ChatColor.DARK_RED + "SERVER TPS UNDER 18.75 GUAKA FIX IT");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                , 300, 300);
     }
 }
