52081c0a1a5b6e074f7c0678f79c6dd8e9c645ac
MonstahGames <monstahhhgames@gmail.com>
2019-06-09 22:14:06 +0200

    Start work on Damage API

---
+++ CroniServer/src/main/java/com/monstahhh/croniserver/plugin/damageapi/events/PlayerDamageEvent.java
@@ -0,0 +1,16 @@
+package com.monstahhh.croniserver.plugin.damageapi.events;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class PlayerDamageEvent implements Listener {
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+    public void onEntityDamage(EntityDamageEvent event) {
+        if (event.getEntityType() == EntityType.PLAYER) {
+            //Do things
+        }
+    }
+}
