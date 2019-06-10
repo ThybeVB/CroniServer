0f46a21d2a7bc01b32a25ca2467f944716d0c3ee
MonstahGames <monstahhhgames@gmail.com>
2019-06-10 14:44:06 +0200

    Work on Damage API

---
+++ CroniServer/src/main/java/com/monstahhh/croniserver/plugin/damageapi/PlayerHandler.java
@@ -0,0 +1,14 @@
+package com.monstahhh.croniserver.plugin.damageapi;
+
+import org.bukkit.entity.Player;
+
+public class PlayerHandler {
+
+    public void setPlayerDamaged (Player player) {
+        //foo
+    }
+
+    public void setPlayerHealthy (Player player) {
+        //foo
+    }
+}
--- CroniServer/src/main/java/com/monstahhh/croniserver/plugin/damageapi/events/PlayerDamageEvent.java
+++ CroniServer/src/main/java/com/monstahhh/croniserver/plugin/damageapi/events/PlayerDamageEvent.java
@@ -1,18 +1,42 @@
 package com.monstahhh.croniserver.plugin.damageapi.events;
 
+import com.monstahhh.croniserver.plugin.damageapi.PlayerHandler;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityRegainHealthEvent;
 
 public class PlayerDamageEvent implements Listener {
+
+    PlayerHandler handler = new PlayerHandler();
+
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
     public void onEntityDamage(EntityDamageEvent event) {
         if (event.getEntityType() == EntityType.PLAYER) {
-            Player p = (Player)event.getEntity();
-
+            Player p = (Player) event.getEntity();
+            if (!isPlayerFullHealth(p)) {
+                handler.setPlayerDamaged(p);
+            }
         }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+    public void onEntityRegainHealth(EntityRegainHealthEvent event) {
+        if (event.getEntityType() == EntityType.PLAYER) {
+            Player p = (Player)event.getEntity();
+            if (isPlayerFullHealth(p)) {
+                handler.setPlayerHealthy(p);
+            }
+        }
+    }
+
+    private boolean isPlayerFullHealth (Player player) {
+        if (player.getHealth() == 20.0) {
+            return true;
+        }
+        return false;
     }
 }
