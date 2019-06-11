72224f77a9f362fe1d313bdecc410be6796eb5db
MonstahGames <monstahhhgames@gmail.com>
2019-06-11 17:44:44 +0200

    Work on Damage API

--- CroniServer/src/main/java/com/monstahhh/croniserver/plugin/damageapi/PlayerHandler.java
+++ CroniServer/src/main/java/com/monstahhh/croniserver/plugin/damageapi/PlayerHandler.java
@@ -3,10 +3,48 @@ package com.monstahhh.croniserver.plugin.damageapi;
 import com.monstahhh.croniserver.plugin.damageapi.configapi.Config;
 import org.bukkit.entity.Player;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class PlayerHandler {
 
+    private List<Player> playersInCombat = new LinkedList<>();
     public void setPlayerInCombat (Player player) {
-        //foo
+        Config playerData = DamageAPI.playerData;
+
+        playerData.getConfig().set("players." + player.getDisplayName() + ".inCombat", true);
+        playerData.saveConfig();
+
+
+        startTimerForPlayer(player);
+    }
+
+    public void setPlayerInNeutral (Player player) {
+        Config playerData = DamageAPI.playerData;
+
+        playerData.getConfig().set("players." + player.getDisplayName() + "inCombat", false);
+        playerData.saveConfig();
+    }
+
+    private void startTimerForPlayer (Player player) {
+        Timer timer = new Timer();
+        if (playersInCombat.contains(player)) {
+            timer.cancel();
+            DamageAPI.debugLog("COMBAT WILL BE RESTARTED FOR " + player.getDisplayName());
+            timer.purge();
+        }
+
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                DamageAPI.debugLog("COMBAT FINISHED FOR " + player.getDisplayName());
+                playersInCombat.remove(player);
+                setPlayerInNeutral(player);
+            }
+        }, 0, 30000);
     }
 
     public void setPlayerDamaged (Player player) {
