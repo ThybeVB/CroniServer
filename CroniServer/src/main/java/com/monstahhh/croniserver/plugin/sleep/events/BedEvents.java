3aad2a84ba50ca5ff61acc920eb73c254ec52cf8
MonstahGames <monstahhhgames@gmail.com>
2019-06-21 21:09:30 +0200

    More Work

--- CroniServer/src/main/java/com/monstahhh/croniserver/plugin/sleep/events/BedEvents.java
+++ CroniServer/src/main/java/com/monstahhh/croniserver/plugin/sleep/events/BedEvents.java
@@ -1,19 +1,45 @@
 package com.monstahhh.croniserver.plugin.sleep.events;
 
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerBedLeaveEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BedEvents implements Listener {
+
+    private List<Player> sleepingPlayers = new ArrayList<>();
 
     @EventHandler
     public void onPlayerBedEnter(PlayerBedEnterEvent event) {
-        //foo
+        if (event.getPlayer().hasPermission("croniserver.sleep")) {
+            Player p = event.getPlayer();
+            sleepingPlayers.add(p);
+            doCheck(event.getPlayer().getWorld());
+        }
     }
 
     @EventHandler
     public void onPlayerBedLeave(PlayerBedLeaveEvent event) {
-        //foo
+        if (event.getPlayer().hasPermission("croniserver.sleep")) {
+            Player p = event.getPlayer();
+            sleepingPlayers.remove(p);
+            doCheck(event.getPlayer().getWorld());
+        }
+    }
+
+    private void doCheck (World w) {
+        if (sleepingPlayers.size() >= 1) {
+            skipNight(w);
+        }
+    }
+
+    private void skipNight (World w) {
+        sleepingPlayers = new ArrayList<>();
+        w.setTime(1000L);
     }
 }
