5723795f7f286f54bb377f0644eae7e5d1bac5d8
MonstahGames <monstahhhgames@gmail.com>
2019-07-03 18:52:58 +0200

    Restructure

---
+++ CroniServer/src/main/java/com/monstahhh/croniserver/plugin/mrworldwide/City.java
@@ -0,0 +1,83 @@
+package com.monstahhh.croniserver.plugin.mrworldwide;
+
+import com.jafregle.http.HttpClient;
+import com.jafregle.http.HttpMethod;
+import com.jafregle.http.HttpResponse;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.MessageEmbed;
+import net.dv8tion.jda.core.entities.TextChannel;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class City {
+
+    private final String baseLink = "http://api.openweathermap.org/data/2.5/weather";
+    private final String params = "?q=%s&appid=%s&units=metric";
+
+    public MessageEmbed getWeatherFor(String providedLocation, String token, TextChannel channel) {
+        try {
+            HttpClient client = new HttpClient();
+            String formattedSend = String.format(params, providedLocation, token);
+            HttpResponse result = client.request(HttpMethod.GET, new StringBuilder(baseLink).append(formattedSend).toString());
+
+            return getEmbedForLocationJson(result.asString());
+
+        } catch (Exception e) {
+            EmbedBuilder eb = new EmbedBuilder();
+            eb.setTitle("Mr. Error");
+            eb.setColor(Color.RED);
+            if (e.getMessage().contains("error: 404")) {
+                eb.addField("Error 404", "The provided city could not be found.", false);
+            } else {
+                eb.addField("Exception", e.getMessage(), false);
+            }
+
+            channel.sendMessage(eb.build()).queue();
+
+            return null;
+        }
+    }
+
+    private MessageEmbed getEmbedForLocationJson(String json) throws JSONException {
+        JSONObject object = new JSONObject(json);
+
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setColor(Color.ORANGE);
+
+        String tempStr = object.getJSONObject("main").get("temp").toString();
+        String minStr = object.getJSONObject("main").get("temp_min").toString();
+        String maxStr = object.getJSONObject("main").get("temp_max").toString();
+
+        int temp = Math.round(Float.parseFloat(tempStr));
+        int min = Math.round(Float.parseFloat(minStr));
+        int max = Math.round(Float.parseFloat(maxStr));
+
+
+        eb.setTitle("Weather for " + object.getString("name") + ", " + object.getJSONObject("sys").getString("country"));
+        eb.addField("Temperature", temp + "°C", false);
+        eb.addField("Minimum & Maximum", min + "°C | " + max + "°C", true);
+
+        Object sunRise = object.getJSONObject("sys").get("sunrise");
+        Date sunRiseDate = new Date(Long.parseLong(sunRise.toString()) * 1000L + (object.getInt("timezone") * 1000L));
+        Object sunSet = object.getJSONObject("sys").get("sunset");
+        Date sunSetDate = new Date(Long.parseLong(sunSet.toString()) * 1000L + (object.getInt("timezone") * 1000L));
+
+        Date current = new Date();
+        current.setTime(current.getTime() + (object.getInt("timezone") * 1000L));
+
+        SimpleDateFormat simpleTime = new java.text.SimpleDateFormat("HH:mm");
+        simpleTime.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+        eb.addField("Sunrise & Sunset", "Sunrise: " + simpleTime.format(sunRiseDate) + " | Sunset: " + simpleTime.format(sunSetDate), false);
+        eb.addField("Current Time", simpleTime.format(current), false);
+
+        eb.setFooter("Crafted with lots of love by Pitbull and OpenWeather API", null);
+
+        return eb.build();
+    }
+}
