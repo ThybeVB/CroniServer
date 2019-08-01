092f7f02e4f4ccfa3fb40480ee15beda40dd3a83
MonstahGames <monstahhhgames@gmail.com>
2019-08-01 20:41:02 +0200

    Better error handling

--- CroniServer/src/main/java/com/monstahhh/croniserver/plugin/mrworldwide/commands/Weather.java
+++ CroniServer/src/main/java/com/monstahhh/croniserver/plugin/mrworldwide/commands/Weather.java
@@ -12,6 +12,10 @@ import java.awt.*;
 
 public class Weather {
 
+    private final EmbedBuilder errorEmbed = new EmbedBuilder()
+            .setTitle("Mr. Error")
+            .setColor(Color.RED);
+
     public void carryCommand(GuildMessageReceivedEvent event, String weatherToken) {
         String providedLoc = (event.getMessage().getContentRaw()).substring(7);
 
@@ -29,20 +33,27 @@ public class Weather {
             }
         } else {
             String possibleCity = this.checkForCity(event);
-            if (!possibleCity.isEmpty()) {
-                WeatherHelper helper = new WeatherHelper(weatherToken, event.getChannel());
-                City city = helper.getWeatherFor(possibleCity);
-                MessageEmbed embed = helper.getEmbedFor(city);
+            try {
+                if (!possibleCity.isEmpty()) {
+                    WeatherHelper helper = new WeatherHelper(weatherToken, event.getChannel());
+                    City city = helper.getWeatherFor(possibleCity);
+                    MessageEmbed embed = helper.getEmbedFor(city);
 
-                if (embed != null) {
-                    event.getChannel().sendMessage(embed).queue();
+                    if (embed != null) {
+                        event.getChannel().sendMessage(embed).queue();
+                    } else {
+                        argError(event, providedLoc);
+                    }
                 } else {
                     argError(event, providedLoc);
                 }
-                return;
+            } catch (NullPointerException e) {
+                EmbedBuilder eb = errorEmbed;
+                eb.addField("Argument Error", "It seems that you did not enter a valid location", false);
+                eb.addField("Notice", "If you want to set your own city, use the setcity command.", false);
+                eb.setFooter("Example: weather london,uk", null);
+                event.getChannel().sendMessage(eb.build()).queue();
             }
-
-            argError(event, providedLoc);
         }
     }
 
@@ -52,9 +63,7 @@ public class Weather {
     }
 
     private void argError(GuildMessageReceivedEvent event, String input) {
-        EmbedBuilder eb = new EmbedBuilder();
-        eb.setTitle("Mr. Error");
-        eb.setColor(Color.RED);
+        EmbedBuilder eb = errorEmbed;
         eb.addField("Argument Error", "It seems that you have not entered the location correctly.", false);
         eb.addField("Example", "weather kortrijk,be", false);
         eb.setFooter("Your Input: weather " + input, null);
