6c60d3922c5f27e00cea811f3584f63c7707b16d
MonstahGames <monstahhhgames@gmail.com>
2020-04-30 17:22:18 +0200

    Finish setprefix command

--- CroniServer/src/main/java/com/monstahhh/croniserver/plugin/mrworldwide/commands/SetPrefix.java
+++ CroniServer/src/main/java/com/monstahhh/croniserver/plugin/mrworldwide/commands/SetPrefix.java
@@ -1,10 +1,42 @@
 package com.monstahhh.croniserver.plugin.mrworldwide.commands;
 
+import com.monstahhh.croniserver.plugin.croniserver.CroniServer;
+import com.monstahhh.croniserver.sqlite.Database;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+import java.awt.*;
 
 public class SetPrefix {
 
+    private final MessageEmbed prefixTooLongErrorEmbed = new EmbedBuilder()
+            .setTitle("Mr. Error")
+            .setColor(Color.RED)
+            .addField("Set prefix error", "The entered prefix is too long. Please try a shorter prefix", false)
+            .build();
+
+    private final MessageEmbed noPrefixErrorEmbed = new EmbedBuilder()
+            .setTitle("Mr. Error")
+            .setColor(Color.RED)
+            .addField("Set prefix error", "Please enter a prefix", false)
+            .build();
+
     public void carryCommand(GuildMessageReceivedEvent event, String cmdStripped) {
-        //foo
+        String prefix = cmdStripped.split(" ")[1];
+
+        if (prefix.isEmpty() || prefix.isBlank()) {
+            event.getChannel().sendMessage(noPrefixErrorEmbed).queue();
+            return;
+        }
+
+        if (prefix.length() > 6) {
+            event.getChannel().sendMessage(prefixTooLongErrorEmbed).queue();
+        } else {
+            Database db = CroniServer._db;
+            db.setPrefix(event.getGuild().getIdLong(), prefix);
+
+            event.getChannel().sendMessage("Server prefix has been updated to ``" + prefix + "``.").queue();
+        }
     }
 }
