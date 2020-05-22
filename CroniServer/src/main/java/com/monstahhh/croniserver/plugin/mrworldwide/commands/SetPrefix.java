08c86b6b251720ae838637ef57210159836f7176
MonstahGames <monstahhhgames@gmail.com>
2020-05-22 23:05:13 +0200

    Add permissions check to setprefix (how did I not do that before omg

--- CroniServer/src/main/java/com/monstahhh/croniserver/plugin/mrworldwide/commands/SetPrefix.java
+++ CroniServer/src/main/java/com/monstahhh/croniserver/plugin/mrworldwide/commands/SetPrefix.java
@@ -3,6 +3,8 @@ package com.monstahhh.croniserver.plugin.mrworldwide.commands;
 import com.monstahhh.croniserver.plugin.croniserver.CroniServer;
 import com.monstahhh.croniserver.sqlite.Database;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
@@ -22,7 +24,21 @@ public class SetPrefix {
             .addField("Set prefix error", "Please enter a prefix", false)
             .build();
 
+    private final MessageEmbed noPermissionsErrorEmbed = new EmbedBuilder()
+            .setTitle("Mr. Error")
+            .setColor(Color.RED)
+            .addField("Set prefix error", "This command requires the ``Administrator`` permission", false)
+            .build();
+
     public void carryCommand(GuildMessageReceivedEvent event, String cmdStripped) {
+        Member m = event.getMessage().getMember();
+        if (m != null) {
+            if (!m.hasPermission(Permission.ADMINISTRATOR)) {
+                event.getChannel().sendMessage(noPermissionsErrorEmbed).queue();
+                return;
+            }
+        }
+
         String prefix = cmdStripped.split(" ")[1];
 
         if (prefix.isEmpty() || prefix.isBlank()) {
